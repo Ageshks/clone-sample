481cc761cfee2ea17294a7aa886b501d84ca7475
Agesh k s <71646123+Ageshks@users.noreply.github.com>
2024-06-04 23:23:07 +0530

    Add files via upload

---
+++ clone/js/script.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function(){
+    $('#carouselExample2Controls').carousel({
+        interval: 1000, // Adjust the interval time as needed
+        pause: 'hover'  // Pause the carousel on hover
+    });
+});
