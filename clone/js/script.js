456097b6f3a2c5aa09c98c4609342728688aa8d9
Agesh k s <71646123+Ageshks@users.noreply.github.com>
2024-06-05 23:04:26 +0530

    Add files via upload

--- clone/js/script.js
+++ clone/js/script.js
@@ -1,8 +1,34 @@
-
-
 $(document).ready(function(){
     $('#carouselExample2Controls').carousel({
         interval: 1000, // Adjust the interval time as needed
         pause: 'hover'  // Pause the carousel on hover
+    });
+});
+
+$(document).ready(function() {
+    const apiKey = '38b24bff3834b33182f5b7d8f496e8df';
+    const url = `https://api.metalpriceapi.com/v1/latest?api_key=${apiKey}&base=USD&currencies=EUR,XAU,XAG`;
+
+    $.ajax({
+        url: url,
+        method: 'GET',
+        success: function(data) {
+            if (data.success) {
+                const goldPrice = data.rates.XAU;
+                $('.container .test').each(function() {
+                    $(this).text(`$${goldPrice.toFixed(2)}`);
+                });
+            } else {
+                $('.container .test').each(function() {
+                    $(this).text('Error getting live gold price.');
+                });
+            }
+        },
+        error: function(error) {
+            console.error('Error fetching the gold price:', error);
+            $('.container .test').each(function() {
+                $(this).text('Error getting live gold price.');
+            });
+        }
     });
 });
